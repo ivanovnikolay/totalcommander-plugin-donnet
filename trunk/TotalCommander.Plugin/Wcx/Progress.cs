066a65cd54ae5557ab5a9df7a19f5f322317aa65
Nikolay Ivanov <Nikolay.Ivanov@ivanov.avsmedia.net>
2011-05-08 18:54:31 +0400

    Progress

--- trunk/TotalCommander.Plugin/Wcx/Progress.cs
+++ trunk/TotalCommander.Plugin/Wcx/Progress.cs
@@ -13,6 +13,22 @@ namespace TotalCommander.Plugin.Wcx
         }
 
 
+        public bool SetProgress(string filename, int unpackedBytes)
+        {
+            return callback(filename, unpackedBytes) != 0;
+        }
+
+        public bool SetFirstProgress(string filename, int percent)
+        {
+            return callback(filename, -percent) != 0;
+        }
+
+        public bool SetSecondProgress(string filename, int percent)
+        {
+            return callback(filename, -1000 - percent) != 0;
+        }
+
+
         internal delegate int Callback(string filename, int size);
     }
 }
